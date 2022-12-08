a35084985709817e2fc9f96bf6d9a187e692e95c
newtonsarker01 <117768263+newtonsarker01@users.noreply.github.com>
2022-12-08 00:06:49 -0600

    Mod-CL-14

---
+++ Class-14.py
@@ -0,0 +1,16 @@
+# print('I am Newton')
+# print("I'm Newton Sarker")
+# I say, "I'm Newton Sarker. I live in Khulna. I went to Cox's Bazar last month."
+# print('I say, "I\'m Newton Sarker. I live in Khulna. I went to Cox\'s Bazar last month."')
+# print("I say, \"I'm Newton Sarker. I live in Khulna. I went to Cox's Bazar last month.\"")
+"""
+Twinkle Twinkle
+Little Star
+How I wonder
+What you are
+"""
+# print("Twinkle Twinkle")
+# print("Little Star")
+# print("How I wonder")
+# print("What you are")
+print("Twinkle Twinkle\n\tLittle Star\n\t\tHow I wonder\n\t\t\tWhat you are")
